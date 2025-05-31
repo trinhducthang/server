dd6da92f0be55f0fb859473018a9e7525295eef2
trinhducthang <trinhducthang.7904@gmail.com>
2025-05-31 15:28:08 +0700

    initialize recruitment details service

---
+++ recruitment-details-service/src/main/java/com/kachina/recruitment_details_service/exception/NotFoundException.java
@@ -0,0 +1,9 @@
+package com.kachina.recruitment_details_service.exception;
+
+public class NotFoundException extends RuntimeException {
+    
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+}
---
+++ recruitment-details-service/src/main/java/com/kachina/recruitment_details_service/jwt/JwtEntryPoint.java
@@ -0,0 +1,42 @@
+package com.kachina.recruitment_details_service.jwt;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kachina.recruitment_details_service.dto.response.ApiResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Date;
+
+@Component
+@Slf4j
+public class JwtEntryPoint implements AuthenticationEntryPoint {
+
+    @Override
+    public void commence(HttpServletRequest request, HttpServletResponse response,
+            AuthenticationException authException) throws IOException, ServletException {
+        log.error("Unauthorized error: {}", authException.getMessage());
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+        ApiResponse body = ApiResponse.<Boolean>builder()
+                .status(HttpServletResponse.SC_UNAUTHORIZED)
+                .error("Unauthorized")
+                .message(authException.getMessage())
+                .result(false)
+                .path(request.getServletPath())
+                .timestamp(new Date())
+                .build();
+
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.writeValue(response.getOutputStream(), body);
+    }
+    
+}
